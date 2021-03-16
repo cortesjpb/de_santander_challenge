76d3545059828c2205fa6344638841d80cf22dcb
Benjamin Cortes <cortesjpb@gmail.com>
2021-03-16 20:26:46 -0300

    refactor to modularize the process, added each function for different tasks

--- transform.py
+++ transform.py
@@ -1,35 +1,104 @@
 import re
 
-# Intentamos abrir el archivo tsv en modo lectura
-try:
-    tsv_file = open("datos_data_engineer.tsv", "rb")
-except IOError:
-    print "El archivo que intenta leer no existe."
+def append_to_line(csv_file, complete_line, column_value, value_index):
+    """
+    Se formatea un valor de una columna y se agrega a la fila que se esta procesando
+    para ser escrita en el CSV.
+    En caso de estar completa la fila (tener 5 campos) escribe en el archivo CSV
+    """
 
-# Abrimos el CSV en el que vamos a escribir
-csv_file = open("datos_data_engineer.csv", "wb")
+    index = len(complete_line)
+    if (index == 5):
+        write_line(csv_file, complete_line)
+        complete_line = []
 
-data = tsv_file.read()
-decoded_data = data.decode("utf-16-le")
-data_formated = re.sub(r" *", "", decoded_data)
-data_formated2 = re.sub(r"\t", "|", data_formated)
-data_formated3 = re.sub(r"\n\|", "|", data_formated2)
-first_part = ""
-for line in data_formated3.split('\n'):
-    delimiter_count = line.count('|')
-    if delimiter_count < 4:
-        if first_part == "":
-            first_part = line
-        else:
-            complete_line = ' '.join([first_part, line])
-            print complete_line
-            first_part = ""
-            csv_file.write(complete_line.encode("utf-8"))
-            csv_file.write('\n'.encode("utf-8"))
+    formated_value = format_column_value(index, column_value)
+    if (value_index == 0) and (0 < index < 4):
+        complete_line[-1] += " " + formated_value
     else:
-        csv_file.write(line.encode("utf-8"))
-        csv_file.write('\n'.encode("utf-8"))
+        complete_line.append(formated_value)
+
+    return complete_line
 
 
-tsv_file.close()
-csv_file.close()
+def format_column_value(index, value):
+    """
+    Formatea los valores de cada columna excluyendo 
+    caracteres que no tienen sentido segun el campo
+    """
+
+    if index in [0,3]:
+        return re.sub('[^0-9]', '', value)
+    elif index in [1,2]:
+        return re.sub('[^\w]', '', value)
+    else:
+        return value
+
+
+def get_csv(file_name):
+    return open(file_name, "wb")
+
+
+def get_formatted_data(file_name):
+    """
+    La funcion realizara el primer formateo general al archivo TSV
+    Quitaremos los espacios, reemplazaremos las tabulaciones por un pipe
+    y finalmente reemplazaremos las ocurrencias de salto de linea seguido de
+    un pipe por un pipe unicamente, en el orden citado
+
+    """
+    try:
+        tsv_file = open(file_name, "rb")
+    except IOError:
+        print "El archivo que intenta leer no existe."
+
+    data = tsv_file.read()
+    decoded_data = data.decode("utf-16-le")
+    data_spaces_deleted = re.sub(r" *", "", decoded_data)
+    data_tab_to_pipe = re.sub(r"\t", "|", data_spaces_deleted)
+    data_newline_to_pipe = re.sub(r"\n\|", "|", data_tab_to_pipe)
+    tsv_file.close()
+    header, data = data_newline_to_pipe.split('\n')[0], data_newline_to_pipe.split('\n')[1:]
+    return header, data
+
+
+def process_data(csv_file, data):
+
+    complete_line = []
+    for line in data:
+        for value_index, column_value in enumerate(line.split('|')):
+            complete_line = append_to_line(csv_file, complete_line, column_value, value_index)
+
+    append_to_line(csv_file, complete_line, "", 0)
+
+
+def write_line(csv_file, complete_line):
+    """
+    Escribimos en el archivo CSV
+    """
+    csv_file.write('|'.join(complete_line).encode("utf-8"))
+    csv_file.write('\n'.encode("utf-8"))
+
+
+def write_header(csv_file, header):
+    csv_file.write(header.encode("utf-8"))
+    csv_file.write('\n'.encode("utf-8"))
+
+
+def main():
+
+    # Formateamos y conseguimos el header y la data por separado
+    header, data = get_formatted_data("datos_data_engineer.tsv")
+
+    # Creamos nuestro CSV y escribimos el header
+    csv_file = get_csv("datos_data_engineer.csv")
+    write_header(csv_file, header)
+
+    # Procesamos y formateamos la data
+    process_data(csv_file, data)
+
+    csv_file.close()
+
+if __name__ == "__main__":
+
+    main()
