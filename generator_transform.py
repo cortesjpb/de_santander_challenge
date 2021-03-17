0c4d4c81e9f5c5a22aa5beed7e148d615d47d7c7
Benjamin Cortes <cortesjpb@gmail.com>
2021-03-17 12:31:32 -0300

    Comments added for explanation

--- generator_transform.py
+++ generator_transform.py
@@ -16,18 +16,25 @@ def format_column_value(row_length, value):
 
 
 def process_header(header):
+    """
+    Damos formato al header de nuestro CSV
+    """
     return '|'.join(header).encode("utf-8")
 
 
 def get_tsv_data(tsv_file_name):
-
+    """
+    Devuelve un generador con las lineas del archivo TSV sin formatear
+    """
     lines = (line for line in io.open(tsv_file_name, mode="r", encoding="utf-16-le"))
     lines_values = (line.rstrip().split('\t') for line in lines)
     return lines_values
 
 
 def process_data(lines_values):
-
+    """
+    Procesamos cada valor de cada una de las filas de un generador
+    """
     row = []
     for line_values  in lines_values:
         index = 0
@@ -56,8 +63,11 @@ def process_data(lines_values):
         final_row = '|'.join(row).encode("utf-8")
         yield final_row
 
-def write_data(csv_file, formatted_header, formatted_data):
 
+def write_data(csv_file, formatted_header, formatted_data):
+    """
+    Escribimos el header y la data formateada a nuestro CSV
+    """
     csv_file.write(formatted_header)
     csv_file.write('\n')
     for row in formatted_data:
@@ -67,20 +77,25 @@ def write_data(csv_file, formatted_header, formatted_data):
 
 def main():
 
+    # Seteamos los nombres de nuestros archivos
     tsv_file_name = "datos_data_engineer.tsv"
     csv_file_name = "datos_data_engineer.csv"
 
+    # Creamos el generador para nuestros datos
     unformatted_data = get_tsv_data(tsv_file_name)    
 
+    # Formateamos la primera linea de nuestros datos, el header
     formatted_header = process_header(next(unformatted_data))
 
+    # Creamos nuestro archivo de salida
     csv_file = open(csv_file_name, "wb")
 
+    # Creamos un generador que formatea los datos
     formatted_data = process_data(unformatted_data)
 
+    # Escribimos nuestros datos formateados en el CSV final
     write_data(csv_file, formatted_header, formatted_data)
 
 
 if __name__ == "__main__":
-
     main()
