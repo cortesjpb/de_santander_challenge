e49b11881eb14c170478e01d17c309d66615df94
Benjamin Cortes <cortesjpb@gmail.com>
2021-03-18 09:06:58 -0300

    name changed, comments updated

--- transform.py
+++ transform.py
@@ -1,121 +1,111 @@
+import io
 import re
 
-def append_to_line(formatted_data, complete_line, column_value, value_index):
+
+def format_column_value(row_length, value):
     """
-    Se formatea un valor de una columna y se agrega a la fila que se esta procesando
-    para ser escrita en el CSV.
-    En caso de estar completa la fila (tener 5 campos) escribe en el archivo CSV
+    Devuelve cada valor de una columna formateado
+    eliminando los caracteres que no deberian estar segun el campo
     """
-
-    index = len(complete_line)
-    if (index == 5):
-        append_line(formatted_data, complete_line)
-        complete_line = []
-
-    if (value_index == 0) and (0 < index < 4):
-        formated_value = format_column_value(index-1, column_value)
-        complete_line[-1] += " " + formated_value
-    else:
-        formated_value = format_column_value(index, column_value)
-        complete_line.append(formated_value)
-
-    return complete_line
-
-
-def format_column_value(index, value):
-    """
-    Formatea los valores de cada columna excluyendo 
-    caracteres que no tienen sentido segun el campo
-    """
-
-    if index in [0,3]:
+    # Si el campo es un ID o un account_number deben ser solo numeros
+    if row_length in [0,3]:
         return re.sub('[^0-9]', '', value)
-    elif index in [1,2]:
+    # Si es un first_name o last_name deberian ser solo letras
+    elif row_length in [1,2]:
         return re.sub('[^\w]', '', value)
     else:
         return value
 
 
-def get_csv(file_name):
-    return open(file_name, "wb")
-
-
-def get_preformatted_data(file_name):
+def process_header(header):
     """
-    La funcion realizara el primer formateo general al archivo TSV
-    Quitaremos los espacios, reemplazaremos las tabulaciones por un pipe
-    y finalmente reemplazaremos las ocurrencias de salto de linea seguido de
-    un pipe por un pipe unicamente, en el orden citado
-
+    Devuelve una sola linea formateada
+    En este caso lo usamos para formatear el header
     """
-    try:
-        tsv_file = open(file_name, "rb")
-    except IOError:
-        print "El archivo que intenta leer no existe."
-
-    data = tsv_file.read()
-    decoded_data = data.decode("utf-16-le")
-    data_spaces_deleted = re.sub(r" *", "", decoded_data)
-    data_tab_to_pipe = re.sub(r"\t", "|", data_spaces_deleted)
-    data_newline_to_pipe = re.sub(r"\n\|", "|", data_tab_to_pipe)
-    tsv_file.close()
-    header, data = data_newline_to_pipe.split('\n')[0], data_newline_to_pipe.split('\n')[1:]
-    return header, data
+    return '|'.join(header).encode("utf-8")
 
 
-def process_values(data):
-
-    formatted_data = []
-    complete_line = []
-    for line in data:
-        for value_index, column_value in enumerate(line.split('|')):
-            complete_line = append_to_line(formatted_data, complete_line, column_value, value_index)
-
-    append_to_line(formatted_data, complete_line, "", 0)
-
-    return formatted_data
-
-
-def append_line(formatted_data, complete_line):
+def get_tsv_data(tsv_file_name):
     """
-    Agregamos la linea formateada a nuestros datos formateados
+    Devuelve un generador con las lineas del archivo TSV sin formatear
     """
-    formatted_data.append('|'.join(complete_line).encode("utf-8"))
-    return formatted_data
+    lines = (line for line in io.open(tsv_file_name, mode="r", encoding="utf-16-le"))
+    lines_values = (line.rstrip().split('\t') for line in lines)
+    return lines_values
 
 
-def write_lines(csv_file, formatted_data):
+def process_data(lines_values):
     """
-    Escribimos en el archivo CSV
+    Devuelve un generador con cada fila procesada
     """
-    for line in formatted_data:
-        csv_file.write(line)
-        csv_file.write('\n'.encode("utf-8"))
+    row = []
+    for line_values  in lines_values:
+        index = 0
+        for value in line_values:
+
+            if (value == '') and (index == 0):
+                continue
+
+            row_length = len(row)
+
+            # Si completamos una row la devolvemos
+            if row_length == 5:
+                final_row = '|'.join(row).encode("utf-8")
+                yield final_row
+                row = []
+            
+            '''Cuando la data tiene un salto de linea tenemos un problema
+            Si la palabra en una nueva linea contiene solo letras 
+            Y recientemente se cargo el first o last name
+            Agregamos el nuevo valor al valor recien cargado
+            Sino simplemente agregamos el valor nuevo a la row'''
+            if (1 < row_length < 4) and (index == 0) and (not value.isnumeric()):
+                formatted_value = format_column_value(row_length-1, value)
+                row[-1] += " " + formatted_value
+            else:
+                formatted_value = format_column_value(row_length, value)
+                row.append(formatted_value)
+            index += 1
+
+    # Aqui procesamos la ultima linea del archivo
+    row_length = len(row)
+    if row_length == 5:
+        final_row = '|'.join(row).encode("utf-8")
+        yield final_row
 
 
-def write_header(csv_file, header):
-    csv_file.write(header.encode("utf-8"))
-    csv_file.write('\n'.encode("utf-8"))
+def write_data(csv_file, formatted_header, formatted_data):
+    """
+    Escribimos el header y la data formateada a nuestro CSV
+    """
+    csv_file.write(formatted_header)
+    csv_file.write('\n')
+    for row in formatted_data:
+        csv_file.write(row)
+        csv_file.write('\n')
 
 
 def main():
 
-    # Formateamos y conseguimos el header y la data por separado
-    header, data = get_preformatted_data("datos_data_engineer.tsv")
+    # Seteamos los nombres de nuestros archivos
+    tsv_file_name = "datos_data_engineer.tsv"
+    csv_file_name = "datos_data_engineer.csv"
 
-    # Creamos nuestro CSV y escribimos el header
-    csv_file = get_csv("datos_data_engineer.csv")
+    # Creamos el generador para nuestros datos
+    unformatted_data = get_tsv_data(tsv_file_name)    
 
-    write_header(csv_file, header)
+    # Formateamos la primera linea de nuestros datos, el header
+    formatted_header = process_header(next(unformatted_data))
 
-    # Procesamos y formateamos la data
-    formatted_data = process_values(data)
+    # Creamos nuestro archivo de salida
+    csv_file = open(csv_file_name, "wb")
 
-    # Escribimos en nuestro CSV
-    write_lines(csv_file, formatted_data)
+    # Creamos un generador que formatea los datos
+    formatted_data = process_data(unformatted_data)
 
-    csv_file.close()
+    # Escribimos nuestros datos formateados en el CSV final
+    write_data(csv_file, formatted_header, formatted_data)
+
 
 if __name__ == "__main__":
-
     main()
